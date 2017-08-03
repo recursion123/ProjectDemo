bba5721ee482dd0e256df2f26bab34c2a1899921
recursion123 <404878319@qq.com>
2017-08-03 11:14:05 +0800

    add dept

--- src/main/java/com/recursion123/BaseApplication.java
+++ src/main/java/com/recursion123/BaseApplication.java
@@ -1,30 +1,36 @@
 package com.recursion123;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.apache.cxf.transport.servlet.CXFServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by zhang on 2016/11/15.
  * BaseApplication
  */
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
-public class BaseApplication extends SpringBootServletInitializer{
-
-//    //部署到外部tomcat
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
+@ComponentScan("com")
+public class BaseApplication extends SpringBootServletInitializer {
+    //    //部署到外部tomcat
 //    @Override
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder config) {
 //        return config.sources(BaseApplication.class);
 //    }
 
+//    @Bean
+//    public ServletRegistrationBean servletRegistrationBean() {
+//        ServletRegistrationBean bean = new ServletRegistrationBean(new CXFServlet(), "/webservice/*");
+//        return bean;
+//    }
+
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(BaseApplication.class,args);
+        SpringApplication.run(BaseApplication.class, args);
     }
 }
--- src/test/java/test5.java
+++ src/test/java/test5.java
@@ -1,13 +1,21 @@
-import com.google.gson.Gson;
-import com.recursion123.model.User;
-import org.junit.Test;
+import com.sun.jna.Native;
+import com.sun.jna.win32.StdCallLibrary;
 
-/**
- * Created by zhang on 2017/7/17.
- */
 public class test5 {
-    @Test
-    public void test(){
+    public interface SiInterface extends StdCallLibrary {
+        SiInterface INSTANCE = (SiInterface) Native.loadLibrary("dll/SiInterface", SiInterface.class);//加载系统kernel32 DLL文件，也可以是C++写的DLL文件
+
+        int INIT(String pErrMsg);
+
+        int BUSINESS_HANDLE(String inputData,
+                            String outputData);
+    }
+
+    public static void main(String[] args) throws Exception {
+        //System.setProperty("jna.encoding","GBK");//设置编码，防止乱码
+
+
+        System.out.println(SiInterface.INSTANCE.INIT("test"));
 
     }
 }
